f96a7747d18461df1a0396c50f527f5e1ade9c01
radoslawjaniak <rajkokret660@gmail.com>
2025-07-02 13:02:41 +0200

    [3 9] update discount.py

--- day_3/discount.py
+++ day_3/discount.py
@@ -14,3 +14,16 @@ print(today.day)  # 2
 # days=0, seconds=0, microseconds=0,
 #                 milliseconds=0, minutes=0, hours=0, weeks=0
 tommorow = today + timedelta(days=1)
+print("Jutro będzie:", tommorow)  # Jutro będzie: 2025-07-03
+
+formated_date = datetime.now().strftime("%d/%m/%Y")
+print("Data sformatowana:", formated_date)  # Data sformatowana: 02/07/2025
+print(type(formated_date))  # <class 'str'>
+
+# sformatowany czas 12:56
+formated_time = datetime.now().strftime("%H:%M")
+print(type(formated_time))
+print("Sformatowany czas:", formated_time)  # Sformatowany czas: 13:00
+
+formated_time_usa = datetime.now().strftime("%I:%M %p")
+print("Czas 12h:", formated_time_usa)  # Czas 12h: 01:01 PM
