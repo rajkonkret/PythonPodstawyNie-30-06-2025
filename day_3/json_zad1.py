64b7aeabc5d4b1e441a52eb03592c78c0d914116
radoslawjaniak <rajkokret660@gmail.com>
2025-07-02 15:29:16 +0200

    [3 15] update json_zad1.py

--- day_3/json_zad1.py
+++ day_3/json_zad1.py
@@ -29,3 +29,25 @@ with open('nasze_dane_sort.json', "w") as f:
 #     "czy_pali": null,
 #     "name": "Radek"
 # }
+
+with open("nasze_dane.json", "r") as fh:
+    data = json.load(fh)
+
+print(data)  # {'name': 'Radek', 'age': 40, 'czy_pali': None}
+print(type(data))  # <class 'dict'>
+
+print(data['name'])  # Radek
+
+json_text = json.dumps(data)
+print(json_text)  # {"name": "Radek", "age": 40, "czy_pali": null}
+print(type(json_text))  # <class 'str'>
+
+dict_json = json.loads(json_text)
+print(dict_json)  # {'name': 'Radek', 'age': 40, 'czy_pali': None}
+print(type(dict_json))  # <class 'dict'>
+
+for k in dict_json:
+    print(k)
+# name
+# age
+# czy_pali
