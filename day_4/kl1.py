1f30380ce5875ff9a6e25d5616b301ca66a7a46c
radoslawjaniak <rajkokret660@gmail.com>
2025-07-03 13:23:53 +0200

    [4 14] add kl1.p

---
+++ day_4/kl1.py
@@ -0,0 +1,22 @@
+# klasa - element programowania obiektowego
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcje w klasie
+# obiekt - instancja klasy
+# klasa musi być zdeklarowana przed użyciem
+# tworzenie obiektu klasy uruchmia metode __init__ w klasie
+# paradygmaty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase -> PEP8
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+
+print(Human.__doc__)  # wypisanie dokumentacji
+# Klasa Human opisująca człowieka w pythonie
+
+# pydoc -b - serwer dokumentacji
+
+# cd day_4
